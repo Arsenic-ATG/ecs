9a174bff01fd39d156d6dbb516ce84ec3fb888f6
Kenneth Gorking <kgorking@outlook.com>
2022-10-23 17:04:48 +0200

    Convert expects/ensures to [[assume]]

--- include/ecs/detail/contract.h
+++ include/ecs/detail/contract.h
@@ -1,7 +1,20 @@
-#ifndef ECS_CONTRACT
-#define ECS_CONTRACT
+#ifndef ECS_DETAIL_CONTRACT_H
+#define ECS_DETAIL_CONTRACT_H
 
-// Contracts. If they are violated, the program is an invalid state, so nuke it from orbit
+#ifdef NDEBUG
+#ifdef __has_cpp_attribute                  // Check if __has_cpp_attribute is present
+#  if __has_cpp_attribute(assume)           // Check for an attribute
+     // Contracts. If they are violated in release, assume 'cond' is correct
+#    define Expects(cond) [[assume(cond)]]
+#    define Ensures(cond) [[assume(cond)]]
+#  else
+#    define Expects(cond) static_cast<void>((cond))
+#    define Ensures(cond) static_cast<void>((cond))
+#  endif
+#endif
+
+#else
+ // Contracts. If they are violated in debug, the program is an invalid state, so nuke it from orbit
 #define Expects(cond)                                                                                                                      \
 	do {                                                                                                                                   \
 		((cond) ? static_cast<void>(0) : std::terminate());                                                                                \
@@ -10,5 +23,6 @@
 	do {                                                                                                                                   \
 		((cond) ? static_cast<void>(0) : std::terminate());                                                                                \
 	} while (false)
+#endif // NDEBUG
 
-#endif // !ECS_CONTRACT
+#endif // !ECS_DETAIL_CONTRACT_H
